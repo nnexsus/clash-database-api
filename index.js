a47be2d276e90d8242ce6ede713d6f8ead1ddedb
nnexsus <nexsusacc@gmail.com>
2022-06-03 17:37:52 -0500

    test deploy 2

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express")
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const db = require('db.json');
+const db = require('./db.json');
 
 const app = express()
 
