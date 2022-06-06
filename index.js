bcb2f2644b89988af1cdc3929a5f0a986ed27765
nnexsus <nexsusacc@gmail.com>
2022-06-06 00:32:19 -0500

    building db & routes update

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express")
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const db = require('./db.json');
+const bdb = require('./bdb.json');
 
 const app = express()
 
@@ -114,6 +115,64 @@ app.get('/api/pet/:id', (req, res) => {
 
 app.get('/api/pets', (req, res) => {
     const troops = db.troops
+    res.send(troops)
+})
+
+app.get('/api/defense/:id', (req, res) => {
+    const troops = bdb.defensive
+    var troopByName = null
+    troops.map((troop) => {
+        if (troop.name === req.params.id) {
+            troopByName = troop
+        }
+    })
+    res.send(troopByName)
+})
+
+app.get('/api/defenses', (req, res) => {
+    const troops = bdb.defensive
+    res.send(troops)
+})
+
+app.get('/api/trap/:id', (req, res) => {
+    const troops = bdb.defensive
+    var troopByName = null
+    troops.map((troop) => {
+        if (troop.name === req.params.id) {
+            troopByName = troop
+        }
+    })
+    res.send(troopByName)
+})
+
+app.get('/api/traps', (req, res) => {
+    const troops = bdb.defensive
+    res.send(troops)
+})
+
+app.get('/api/army/:id', (req, res) => {
+    const troops = bdb.army
+    var troopByName = null
+    troops.map((troop) => {
+        if (troop.name === req.params.id) {
+            troopByName = troop
+        }
+    })
+    res.send(troopByName)
+})
+
+app.get('/api/army', (req, res) => {
+    const troops = bdb.army
+    res.send(troops)
+})
+
+app.get('/api/types', (req, res) => {
+    const troops = bdb.types
+    res.send(troops)
+})
+
+app.get('/api/walls', (req, res) => {
+    const troops = bdb.walls
     res.send(troops)
 })
 
